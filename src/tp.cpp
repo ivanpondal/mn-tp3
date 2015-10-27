3ae797bfad4ddcc051b1709fcf5c0d09425e6e02
Iván Pondal <ivan.pondal@gmail.com>
2015-10-27 14:14:28 -0300

    Add spline interpolator tests

--- src/tp.cpp
+++ src/tp.cpp
@@ -1,4 +1,8 @@
+#define DELTA 0.0001
+#define DEBUG false
+
 #include "mini_test.h"
+#include "spline.h"
 //#include "utils.h"
 
 #include <string>
@@ -25,6 +29,37 @@ double stop_timer() {
 }
 
 // ****************************** AUXILIARES ***********************************
+
+void assert_precision(double valor, double esperado, double precision){
+	ASSERT(abs(valor - esperado) < precision);
+}
+
+void assert_precision(double valor, double esperado){
+	assert_precision(valor, esperado, DELTA);
+}
+
+void assert_interpolacion_spline(Spline spline, vector<double> esperados, double intervalo, double precision){
+	double xj = 0;
+	for(unsigned int i = 0; i < esperados.size(); i++){
+		if(DEBUG){
+			cout << "S" << i << "(" << xj << ") = " << spline.evaluar(xj)<< endl;
+		}
+		// Si el intervalo equivale a un punto interpolado, el spline TIENE que
+		// darme el "mismo" valor que la función original
+		if(abs(xj - i) < DELTA){
+			assert_precision(spline.evaluar(xj), esperados[i]);
+		}
+		else{
+			assert_precision(spline.evaluar(xj), esperados[i], precision);
+		}
+		xj += intervalo;
+	}
+}
+
+void assert_interpolacion_spline(Spline spline, vector<double> esperados, double intervalo){
+	assert_interpolacion_spline(spline, esperados, intervalo, DELTA);
+}
+
 void video_a_texto(const char* videofile, const char* textfile, int salto = 1) {
     char command[1024];
     sprintf(command, "octave --path tools/ --eval \"source('tools/videoToTextfile.m'); videoToTextfile('%s', '%s', %d);\" >> /dev/null",
@@ -48,6 +83,30 @@ void test_texto_a_video() {
     texto_a_video("data/baby.txt","data/baby_re.avi");
 }
 
+// f(x) = 42
+void test_spline_constante(){
+	vector<double> y = {42, 42, 42, 42};
+	vector<double> esperados = {42, 42, 42, 42, 42, 42, 42};
+	Spline spline(y);
+	assert_interpolacion_spline(spline, esperados, 0.5);
+}
+
+// f(x) = x
+void test_spline_lineal(){
+	vector<double> y = {0, 1, 2, 3};
+	vector<double> esperados = {0, 0.5, 1, 1.5, 2, 2.5, 3};
+	Spline spline(y);
+	assert_interpolacion_spline(spline, esperados, 0.5);
+}
+
+// f(x) = x^2
+void test_spline_cuadratico(){
+	vector<double> y = {0, 1, 4, 9};
+	vector<double> esperados = {0, 0.25, 1, 2.25, 4, 6.25, 9};
+	Spline spline(y);
+	assert_interpolacion_spline(spline, esperados, 0.5, 0.5);
+}
+
 // ****************** FUNCION PARA FORMATO DE LA CATEDRA ***********************
 void resolver(const char* inputfile, const char* outputfile, int metodo, int cuadros) {
     cout << "resolver" << endl;
@@ -68,6 +127,9 @@ int main(int argc, char *argv[])
 		resolver(inputfile, outputfile, metodo, cuadros);
 	} else if(argc == 1){
         // tests grupo
+		RUN_TEST(test_spline_constante);
+		RUN_TEST(test_spline_lineal);
+		RUN_TEST(test_spline_cuadratico);
         test_video_a_texto();
         test_texto_a_video();
 	} else {
