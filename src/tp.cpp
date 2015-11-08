f32ae223d601f54bb7c4412c708feb6953f39986
Ivan Arcuschin <iarcuschin@gmail.com>
2015-11-08 20:34:35 -0300

    Muevo metodos auxiliares de test_utils a otro archivo

--- src/tp.cpp
+++ src/tp.cpp
@@ -1,14 +1,13 @@
 #define DELTA 0.0001
 #define DEBUG false
 
-#include "mini_test.h"
 #include "interpolador.h"
 #include "vecinos.h"
 #include "lineal.h"
 #include "spline.h"
 #include "multi_spline.h"
 #include "video.h"
-//#include "utils.h"
+#include "test_utils.h"
 
 #include <string>
 #include <sstream>
@@ -21,7 +20,7 @@
 #include <iomanip>
 
 using namespace std;
-//using namespace utils;
+using namespace utils;
 
 // ****************************** CHRONO ***************************************
 static chrono::time_point<chrono::high_resolution_clock> start_time;
@@ -35,245 +34,6 @@ double stop_timer() {
     return double(chrono::duration_cast<chrono::nanoseconds>(end_time - start_time).count());
 }
 
-// ****************************** AUXILIARES ***********************************
-
-void assert_precision(double valor, double esperado, double precision){
-	ASSERT(abs(valor - esperado) < precision);
-}
-
-void assert_precision(double valor, double esperado){
-	assert_precision(valor, esperado, DELTA);
-}
-
-void assert_interpolacion(Interpolador* interpolador, vector<double> esperados, double intervalo, double precision){
-	double xj = 0;
-	for(unsigned int i = 0; i < esperados.size(); i++){
-		if(DEBUG){
-			cout << "P(" << xj << ") = " << interpolador->evaluar(xj) << ", Expected(" << xj <<") = " << esperados[i] << endl;
-		}
-		// Si el intervalo equivale a un punto interpolado, el interpolador TIENE que
-		// darme el "mismo" valor que la función original
-		if(abs(xj - i) < DELTA){
-			assert_precision(interpolador->evaluar(xj), esperados[i]);
-		} else{
-			assert_precision(interpolador->evaluar(xj), esperados[i], precision);
-		}
-		xj += intervalo;
-	}
-}
-
-void assert_interpolacion(Interpolador* interpolador, vector<double> esperados, double intervalo){
-	assert_interpolacion(interpolador, esperados, intervalo, DELTA);
-}
-
-double frame_error_cuadratico_medio(const vector<vector<int > > &output, const vector<vector<int > > &real) {
-    ASSERT(output.size() != 0 && output[0].size() != 0);
-    ASSERT(real.size() != 0 && real[0].size() != 0);
-    ASSERT(output.size() == real.size() && output[0].size() == real[0].size());
-
-    int ancho = output.size();
-    int alto = output[0].size();
-    unsigned long sum = 0;
-    for(int x = 0; x < ancho; x++){
-		for(int y = 0; y < alto; y++){
-            sum += pow(real[x][y] - output[x][y], 2);
-        }
-    }
-    return double(sum)/(double(ancho)*double(alto));
-}
-
-double frame_peak_to_signal_noise_ratio(const vector<vector<int > > &output, const vector<vector<int > > &real) {
-    ASSERT(output.size() != 0 && output[0].size() != 0);
-    ASSERT(real.size() != 0 && real[0].size() != 0);
-    ASSERT(output.size() == real.size() && output[0].size() == real[0].size());
-
-    double aux = double(pow(255,2));
-    double ecm = frame_error_cuadratico_medio(output, real);
-    if (ecm < DELTA) {
-        // el frame output es igual al frame real
-        return 0;
-    } else {
-        return 10*log10(aux/ecm);
-    }
-}
-
-double video_max_error_cuadratico_medio(const vector<vector<vector<int> > > &output, const vector<vector<vector<int> > > &real) {
-    ASSERT(output.size() != 0 && output[0].size() != 0 && output[0][0].size() != 0);
-    ASSERT(real.size() != 0 && real[0].size() != 0 && real[0][0].size() != 0);
-    ASSERT(output.size() == real.size() && output[0].size() == real[0].size() && output[0][0].size() <= real[0][0].size());
-
-    int ancho = output.size();
-    int alto = output[0].size();
-    int frames = output[0][0].size();
-    double max_error = 0;
-
-    vector<vector<int > > frame_output(ancho, vector<int>(alto, 0));
-    vector<vector<int > > frame_real(ancho, vector<int>(alto, 0));
-    for (int k = 0; k < frames; k++) {
-        for(int x = 0; x < ancho; x++){
-    		for(int y = 0; y < alto; y++){
-                frame_output[x][y] = output[x][y][k];
-                frame_real[x][y] = real[x][y][k];
-            }
-        }
-        double error = frame_error_cuadratico_medio(frame_output, frame_real);
-        if (error > max_error)  {
-            max_error = error;
-        }
-    }
-
-    return max_error;
-}
-
-double video_max_peak_to_signal_noise_ratio(const vector<vector<vector<int> > > &output, const vector<vector<vector<int> > > &real) {
-    ASSERT(output.size() != 0 && output[0].size() != 0 && output[0][0].size() != 0);
-    ASSERT(real.size() != 0 && real[0].size() != 0 && real[0][0].size() != 0);
-    ASSERT(output.size() == real.size() && output[0].size() == real[0].size() && output[0][0].size() <= real[0][0].size());
-
-    int ancho = output.size();
-    int alto = output[0].size();
-    int frames = output[0][0].size();
-    double max_error = 0;
-
-    vector<vector<int > > frame_output(ancho, vector<int>(alto, 0));
-    vector<vector<int > > frame_real(ancho, vector<int>(alto, 0));
-    for (int k = 0; k < frames; k++) {
-        for(int x = 0; x < ancho; x++){
-    		for(int y = 0; y < alto; y++){
-                frame_output[x][y] = output[x][y][k];
-                frame_real[x][y] = real[x][y][k];
-            }
-        }
-        double error = frame_peak_to_signal_noise_ratio(frame_output, frame_real);
-        if (error > max_error)  {
-            max_error = error;
-        }
-    }
-
-    return max_error;
-}
-
-double video_prom_error_cuadratico_medio(const vector<vector<vector<int> > > &output, const vector<vector<vector<int> > > &real, vector<int>* resultados) {
-    ASSERT(output.size() != 0 && output[0].size() != 0 && output[0][0].size() != 0);
-    ASSERT(real.size() != 0 && real[0].size() != 0 && real[0][0].size() != 0);
-    ASSERT(output.size() == real.size() && output[0].size() == real[0].size() && output[0][0].size() <= real[0][0].size());
-
-    int ancho = output.size();
-    int alto = output[0].size();
-    int frames = output[0][0].size();
-    int frames_iguales = 0;
-    double sum_error = 0;
-
-    vector<vector<int > > frame_output(ancho, vector<int>(alto, 0));
-    vector<vector<int > > frame_real(ancho, vector<int>(alto, 0));
-    for (int k = 0; k < frames; k++) {
-        for(int x = 0; x < ancho; x++){
-    		for(int y = 0; y < alto; y++){
-                frame_output[x][y] = output[x][y][k];
-                frame_real[x][y] = real[x][y][k];
-            }
-        }
-        double error = frame_error_cuadratico_medio(frame_output, frame_real);
-        if (error < DELTA) {
-            // el frame output es igual al frame real
-            frames_iguales++;
-        } else {
-            sum_error += error;
-        }
-        resultados[k] = (int) error;
-    }
-
-    cout << "Hay " << frames_iguales << " frames iguales de " << frames << ", o: " << setprecision(4) << double(frames_iguales)/double(frames) << "%" << endl;
-
-    return sum_error/double(frames - frames_iguales);
-}
-
-double video_prom_peak_to_signal_noise_ratio(const vector<vector<vector<int> > > &output, const vector<vector<vector<int> > > &real) {
-    ASSERT(output.size() != 0 && output[0].size() != 0 && output[0][0].size() != 0);
-    ASSERT(real.size() != 0 && real[0].size() != 0 && real[0][0].size() != 0);
-    ASSERT(output.size() == real.size() && output[0].size() == real[0].size() && output[0][0].size() <= real[0][0].size());
-
-    int ancho = output.size();
-    int alto = output[0].size();
-    int frames = output[0][0].size();
-    int frames_iguales = 0;
-    double sum_error = 0;
-
-    vector<vector<int > > frame_output(ancho, vector<int>(alto, 0));
-    vector<vector<int > > frame_real(ancho, vector<int>(alto, 0));
-    for (int k = 0; k < frames; k++) {
-        for(int x = 0; x < ancho; x++){
-    		for(int y = 0; y < alto; y++){
-                frame_output[x][y] = output[x][y][k];
-                frame_real[x][y] = real[x][y][k];
-            }
-        }
-        double error = frame_peak_to_signal_noise_ratio(frame_output, frame_real);
-        if (error < DELTA) {
-            // el frame output es igual al frame real
-            frames_iguales++;
-        } else {
-            sum_error += error;
-        }
-    }
-
-    cout << "Hay " << frames_iguales << " frames iguales de " << frames << ", o: " << setprecision(4) << double(frames_iguales)/double(frames) << "%" << endl;
-
-    return sum_error/double(frames - frames_iguales);
-}
-
-void error_cuadratico_medio_per_frame(const vector<vector<vector<int> > > &output, const vector<vector<vector<int> > > &real) {
-    /*
-    ASSERT(output.size() != 0 && output[0].size() != 0 && output[0][0].size() != 0);
-    ASSERT(real.size() != 0 && real[0].size() != 0 && real[0][0].size() != 0);
-    ASSERT(output.size() == real.size() && output[0].size() == real[0].size() && output[0][0].size() <= real[0][0].size());
-    */
-    int ancho = output.size();
-    int alto = output[0].size();
-    int frames = output[0][0].size();
-    int frames_iguales = 0;
-    double sum_error = 0;
-
-    vector<vector<int > > frame_output(ancho, vector<int>(alto, 0));
-    vector<vector<int > > frame_real(ancho, vector<int>(alto, 0));
-    for (int k = 0; k < frames; k++) {
-        for(int x = 0; x < ancho; x++){
-    		for(int y = 0; y < alto; y++){
-                frame_output[x][y] = output[x][y][k];
-                frame_real[x][y] = real[x][y][k];
-            }
-        }
-        double error = frame_error_cuadratico_medio(frame_output, frame_real);
-        if (error < DELTA) {
-            // el frame output es igual al frame real
-            frames_iguales++;
-        } else {
-            sum_error += error;
-        }
-    }
-
-    cout << "Hay " << frames_iguales << " frames iguales de " << frames << ", o: " << setprecision(4) << double(frames_iguales)/double(frames) << "%" << endl;
-
-    return sum_error/double(frames - frames_iguales);
-}
-
-
-void video_a_texto(const char* videofile, const char* textfile, int salto = 1) {
-    cout << "Convirtiendo video " << videofile << " a texto " <<  textfile << " con salto " << salto << endl;
-    char command[1024];
-    sprintf(command, "python tools/videoToTextfile.py %s %s %d >> /dev/null",
-        videofile, textfile, salto);
-    if(system(command)) { cout << "videoToTextfile failed" << endl; };
-}
-
-void texto_a_video(const char* textfile, const char* videofile) {
-    cout << "Convirtiendo texto " << textfile << " a video " <<  videofile << endl;
-    char command[1024];
-    sprintf(command, "python tools/textfileToVideo.py %s %s >> /dev/null",
-        textfile, videofile);
-    if(system(command)) { cout << "textfileToVideo failed" << endl; };
-}
-
 // ************************* TESTS DEL GRUPO ***********************************
 void test_video_a_texto() {
     video_a_texto("data/baby.avi","data/baby.txt");
@@ -282,7 +42,6 @@ void test_video_a_texto() {
 void test_texto_a_video() {
     texto_a_video("data/baby.txt","data/baby_re.avi");
 }
-
 
 // f(x) = 42
 void test_vecinos_constante(){
